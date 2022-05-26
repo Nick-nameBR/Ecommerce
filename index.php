99ad3b1d1557e53430f6d4869ce0c387b47d0d88
nikolas <nikolasmvsantos@gmail.com>
2022-05-26 11:52:22 -0300

    criando rotas classe Page

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php 
 
-require_once("vendor/autoload.php");
+	require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+	use \Slim\Slim;
+	use \Hcode\Page;
 
-$app->config('debug', true);
+	$app = new Slim();
 
-$app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
+	$app->config('debug', true);
 
-	$result = $sql->select("SELECT*FROM tb_users" );
+	$app->get('/', function() {
+		
+		$page = new Page();
 
-	echo json_encode($result);
+		$page->setTpl("index");
 
-});
+	});
 
-$app->run();
+	$app->run();
 
  ?>
