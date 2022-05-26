f1d2c331f4512052bed7ca46a83e9ce3af3161ac
nikolas <nikolasmvsantos@gmail.com>
2022-05-26 15:36:52 -0300

    TPL admin

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 
 	require_once("vendor/autoload.php");
 
+	
 	use \Slim\Slim;
 	use \Hcode\Page;
+	use \Hcode\PageAdmin;
 
 	$app = new Slim();
 
@@ -12,6 +14,15 @@
 	$app->get('/', function() {
 		
 		$page = new Page();
+
+		$page->setTpl("index");
+
+	});
+
+
+	$app->get('/admin/', function() {
+		
+		$page = new PageAdmin();
 
 		$page->setTpl("index");
 
--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -3,7 +3,7 @@
     namespace Hcode;
 
     use Rain\Tpl;
-
+    
     class Page {
 
         private $tpl;
@@ -12,12 +12,12 @@
             "data"=>[]
         ];
 
-        public function __construct($opts = array()){
+        public function __construct($opts = array(), $tpl_dir = "/views/"){
 
             $this->options = array_merge($this->default, $opts);
 
             $config = array(
-                "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+                "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
                 "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
                 "debug"         => false
             );
