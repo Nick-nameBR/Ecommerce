c4fb03b3060b6371fb9243146cdbbcba247e69fa
nikolas <nikolasmvsantos@gmail.com>
2022-06-02 15:08:58 -0300

    paginação

--- site.php
+++ site.php
@@ -3,6 +3,11 @@
     use \Hcode\Page;
     use \Hcode\Model\Category;
     use \Hcode\Model\Product;
+    use \Hcode\Model\Cart;
+    use \Hcode\Model\Address;
+    use \Hcode\Model\User;
+    use \Hcode\Model\Order;
+    use \Hcode\Model\OrderStatus;
 
     $app->get('/', function() {
 
@@ -16,17 +21,48 @@
 
     });
 
-    $app->get("/categories/:idcategory",function($idcategory){
+    $app->get("/categories/:idcategory/", function($idcategory){
 
-		$category= new Category();
-		$category->get((int)$idcategory);
-
-		$page = new Page();
-
-		$page->setTpl("category", [
-			'category'=>$category->getValeus(),
-			'products'=>Product::checkList($category->getProducts())
-		]);
-	});
+        $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+    
+        $category = new Category();
+    
+        $category->get((int)$idcategory);
+    
+        $pagination = $category->getProductsPage($page);
+    
+        $pages = [];
+    
+        for ($i=1; $i <= $pagination['pages']; $i++) { 
+            array_push($pages, [
+                'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+                'page'=>$i
+            ]);
+        }
+    
+        $page = new Page();
+    
+        $page->setTpl("category", [
+            'category'=>$category->getValues(),
+            'products'=>$pagination["data"],
+            'pages'=>$pages
+        ]);
+    
+    });
+    
+    $app->get("/products/:desurl/", function($desurl){
+    
+        $product = new Product();
+    
+        $product->getFromURL($desurl);
+    
+        $page = new Page();
+    
+        $page->setTpl("product-detail", [
+            'product'=>$product->getValues(),
+            'categories'=>$product->getCategories()
+        ]);
+    
+    });
 
 ?>
